ebd7823f357111aad241d46a379b8a6181e66b8d
Leo <leo.battalora@gmail.com>
2024-08-29 23:03:19 -0400

    updated site description

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,22 @@ export default function Home() {
     <div className="grow flex flex-col items-center justify-between">
       <div className="pb-2 w-full flex flex-col items-center">
         {/* about */}
-        <div className="pt-14 pb-20 pl-8 px-8 w-full flex grow max-w-4xl items-center text-sm">
+        <div className="pt-14 pb-20 pl-12 px-8 w-full flex grow max-w-4xl items-center text-sm">
           <div className="w-1/3">
             <Image src="/wburgbr_4x3_color.jpg" alt="williamsburg bridge" width={600} height={600} />
           </div>
-          <div className="w-2/3 pl-8 -mt-6 flex flex-col items-center justify-between space-y-3">
+          <div className="w-2/3 pl-8 flex flex-col items-center justify-between space-y-3">
             <h2 className="w-full text-2xl font-semibold">About</h2>
             <p className="w-full">
-              Fifty-Three Pitt is a boutique investment firm which specializes in equity research and financial
-              education. With nearly 2 days of experience in the financial industry, we have a proven track record of
-              delivering consistently middling results for our clients.
+              Fifty-Three Pitt is a boutique investment firm specializing in equity research and financial education. We
+              find high-octane giga-compounder stonks and make them more expensive for all you libtard retail traders by
+              infusing our client&apos;s hard-earned capital into their valuations.
+            </p>
+            <p className="w-full">
+              Our star-studded team of Nigerian quants start each morning by declineing multi-million dollar sign-on
+              bonuses from Gayman Sachs and Mogran Stans. Fifty-Three Pitt engages in max-alpha statistical arbitrage by
+              being the proud counterparty to every trade Jane Boulevard&apos;s dares to enter into their Robinhood
+              account.
             </p>
           </div>
         </div>
--- app/research/page.tsx
+++ app/research/page.tsx
@@ -20,14 +20,6 @@ export default function Research() {
       time: "12:00 EST",
     },
     {
-      title: "Equity Research: Tesla Inc. (TSLA)",
-      description:
-        "When generative AI first emerged, people mostly used it for simple things like creating recipes or planning trips. But as they became more familiar with what it could do, they’ve also grown more...",
-      thumbnail: "/article-thumbnails/002.jpg",
-      date: "2024-01-01",
-      time: "12:00 EST",
-    },
-    {
       title: "From Bots to Agents: The Next Great Leap Forward Is Autonomous AI",
       description:
         "When generative AI first emerged, people mostly used it for simple things like creating recipes or planning trips. But as they became more familiar with what it could do, they’ve also grown more...",
@@ -44,20 +36,20 @@ export default function Research() {
         <div className="p-14 px-8 w-full flex flex-col max-w-4xl text-sm space-y-8">
           {articles.map((article, idx) => (
             <div key={idx} className="w-full flex flex-col items-center">
-              <Link href="/">
-                <div className="pb-8 w-full flex items-center justify-between space-x-4">
-                  <div className="w-1/4 pl-4">
-                    <Image src={article.thumbnail} alt={article.thumbnail} width={300} height={300} />
-                  </div>
-                  <div className="w-3/4 flex flex-col">
-                    <h2 className="text-xl font-semibold">{article.title}</h2>
-                    <p className="italic pb-2">
-                      {article.date} {article.time}
-                    </p>
-                    <p className="">{article.description}</p>
-                  </div>
+              {/* <Link href="/"> */}
+              <div className="pb-8 w-full flex items-center justify-between space-x-4">
+                <div className="w-1/4 pl-4">
+                  <Image src={article.thumbnail} alt={article.thumbnail} width={300} height={300} />
                 </div>
-              </Link>
+                <div className="w-3/4 flex flex-col">
+                  <h2 className="text-xl font-semibold">{article.title}</h2>
+                  <p className="italic pb-2">
+                    {article.date} {article.time}
+                  </p>
+                  <p className="">{article.description}</p>
+                </div>
+              </div>
+              {/* </Link> */}
               {idx !== articles.length - 1 && <div className="max-w-2xl border-t border-gray-800 w-16" />}
             </div>
           ))}
